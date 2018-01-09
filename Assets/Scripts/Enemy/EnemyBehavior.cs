7dc62af71ff4ba9a42c144f142cb3f10aec981a8
Denis Yelchaninov <11614834+Dwarfo@users.noreply.github.com>
2018-01-10 00:13:43 +0200

    Started working on AI
    It faces and chases player, yet chasing needs to be made more interesting
    
    To face properly the rotation applied by forces need to be nullified via huge AngularDrag, otherwise it adds up to the script rotation.

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -6,21 +6,34 @@ using UnityEngine.AI;
 public class EnemyBehavior : MonoBehaviour {
 
     public float aggroRadius = 10f;
-    public Transform player;
+    public int speedKoef = 2;
+    public float topSpeed = 8f;
+    public int dragValue = 8;
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    private Transform targetObject;
+    private Rigidbody2D enemy;
+
+
+    void Start ()
+    {
+        //Rigidbody has to have huge angular drag in order to face target properly
+        //if not the rotation applied to object sums up with rotation provided by script
+        gameObject.GetComponent<Rigidbody2D>().angularDrag = 10;
+        enemy = gameObject.GetComponent<Rigidbody2D>();	
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
-        float distance = Vector2.Distance(player.position, transform.position);
-        if (distance <= aggroRadius)
+        if (targetObject != null)
         {
-            lookAtPlayer();
+            enemy.drag = 0;
+            Vector2 direction = (targetObject.position - transform.position).normalized;
+            lookAtTarget(direction);
+            chaseTarget(direction);
         }
+        else
+            enemy.drag = dragValue;
 	}
 
     private void OnDrawGizmosSelected()
@@ -29,16 +42,56 @@ public class EnemyBehavior : MonoBehaviour {
         Gizmos.DrawWireSphere(transform.position,aggroRadius);
     }
 
-    private void lookAtPlayer()
+    private void lookAtTarget(Vector2 direction)
     {
-        Vector2 direction = (player.position - transform.position).normalized;
-        //Quaternion lookAt = Quaternion.LookRotation(direction);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, lookAt, Time.deltaTime * 5f);
-
         float rotation_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, rotation_z), Time.deltaTime * 5f);
+        Quaternion rotation = Quaternion.AngleAxis(rotation_z, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
     }
 
+    private void chaseTarget(Vector2 direction)
+    {
+        float distance = Vector2.Distance(gameObject.transform.position, targetObject.position);
+        float koef;
+        if (distance >= 5f)
+        {
+            koef = topSpeed;
+        }
+        else
+        {
+            koef = -topSpeed;
+        }
+        enemy.AddForce(direction * koef);
+        enemy.velocity = clampVelocity(speedKoef);
+    }
 
+    private Vector2 clampVelocity(int modeKoef)
+    {
+        // clamp between topSpeed negative and positive values depending on mode
+        float x = Mathf.Clamp(enemy.velocity.x, -topSpeed / modeKoef, topSpeed / modeKoef);
+        float y = Mathf.Clamp(enemy.velocity.y, -topSpeed / modeKoef, topSpeed / modeKoef);
+
+        return new Vector2(x, y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("I see ");
+        Transform target = collision.GetComponent<Transform>();
+        if (collision.gameObject.layer == 8)
+        {
+            //Debug.Log("I see Player");
+            targetObject = target;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Transform target = collision.GetComponent<Transform>();
+        if (collision.gameObject.layer == 8)
+        {
+            targetObject = null;
+        }
+    }
 
 }
